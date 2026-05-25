e2ffb0189d192e7dbb3dd2c8b8088042fb818254
Anna Ufimtseva <ufimtseva.anna.al@gmail.com>
2026-05-25 23:55:00 +0500

    add sort types

--- src/const.js
+++ src/const.js
@@ -1,4 +1,4 @@
-export const TYPES = [
+const TYPES = [
   'taxi',
   'bus',
   'train',
@@ -10,30 +10,40 @@ export const TYPES = [
   'restaurant'
 ];
 
-export const CITIES = [
-  'Amsterdam',
-  'Geneva',
-  'Chamonix',
-  'London',
-  'Paris',
-  'Berlin',
-  'Rome',
-  'Barcelona',
-  'Prague',
-  'Vienna'
+const OFFERS = [
+  {
+    id: 1,
+    title: 'Add luggage',
+    price: 30
+  },
+  {
+    id: 2,
+    title: 'Switch to comfort',
+    price: 100
+  },
+  {
+    id: 3,
+    title: 'Add meal',
+    price: 15
+  }
 ];
 
-export const OFFERS = [
-  { id: 'offer-1', title: 'Add luggage', price: 50 },
-  { id: 'offer-2', title: 'Switch to comfort class', price: 80 },
-  { id: 'offer-3', title: 'Add meal', price: 15 },
-  { id: 'offer-4', title: 'Choose seats', price: 10 },
-  { id: 'offer-5', title: 'Travel insurance', price: 30 }
-];
-
-export const FilterType = {
+const FilterType = {
   EVERYTHING: 'everything',
   FUTURE: 'future',
   PRESENT: 'present',
   PAST: 'past'
+};
+
+const SortType = {
+  DAY: 'day',
+  TIME: 'time',
+  PRICE: 'price'
+};
+
+export {
+  TYPES,
+  OFFERS,
+  FilterType,
+  SortType
 };
